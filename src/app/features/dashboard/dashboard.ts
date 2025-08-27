71404646f7f4e3625e95861126a614fb341d7c64
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-08-27 16:50:48 +0000

    corrected formatting so that ui renders data.json

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,8 @@
-import { Component, type OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core'
-import { AlertService } from '../../core/services/alert.service'
-import type { AlertViewModel } from '../../core/models/alert-view.model'
-import { CommonModule } from '@angular/common'
-import { AlertListComponent } from '../alert-list/alert-list'
+import { Component, type OnInit, CUSTOM_ELEMENTS_SCHEMA, signal, WritableSignal } from '@angular/core';
+import { AlertService } from '../../core/services/alert.service';
+import type { AlertViewModel } from '../../core/models/alert-view.model';
+import { CommonModule } from '@angular/common';
+import { AlertListComponent } from '../alert-list/alert-list';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,31 +13,32 @@ import { AlertListComponent } from '../alert-list/alert-list'
   styleUrl: './dashboard.css',
 })
 export class DashboardComponent implements OnInit {
-  currentAlerts: AlertViewModel[] = []
-  selectedAlert: AlertViewModel | null = null
-  dialogOpen = false
+  currentAlerts: WritableSignal<AlertViewModel[]> = signal([]);
+  selectedAlert: AlertViewModel | null = null;
+  dialogOpen = false;
 
   constructor(private alertService: AlertService) {}
 
   ngOnInit(): void {
     this.alertService.getAlerts().subscribe({
       next: (alerts: AlertViewModel[]) => {
-        this.currentAlerts = alerts.sort((a, b) => b.errorTime - a.errorTime)
-        console.log('Received and sorted alerts:', this.currentAlerts)
+        const sortedAlerts = alerts.sort((a, b) => b.errorTime - a.errorTime);
+        this.currentAlerts.set(sortedAlerts);
+        console.log('Received and sorted alerts:', this.currentAlerts());
       },
       error: (err) => {
-        console.error('Error fetching alerts:', err)
+        console.error('Error fetching alerts:', err);
       },
-    })
+    });
   }
 
   onShowDetails(alert: AlertViewModel): void {
-    this.selectedAlert = alert
-    this.dialogOpen = true
+    this.selectedAlert = alert;
+    this.dialogOpen = true;
   }
 
   onDialogClosed(): void {
-    this.dialogOpen = false
-    this.selectedAlert = null
+    this.dialogOpen = false;
+    this.selectedAlert = null;
   }
 }
